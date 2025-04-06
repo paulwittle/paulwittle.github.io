9b105c6678ff398ca51c10d99a6f65321537626d
Paul Wittle <paul_wittle@hotmail.com>
2025-04-06 21:19:12 +0100

    Update references

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css';
-import Map from 'node_modules/ol/Map.js';
-import OSM from 'node_modules/ol/source/OSM.js';
-import TileLayer from 'node_modules/ol/layer/Tile.js';
-import View from 'node_modules/ol/View.js';
+import Map from './node_modules/ol/Map.js';
+import OSM from './node_modules/ol/source/OSM.js';
+import TileLayer from './node_modules/ol/layer/Tile.js';
+import View from './node_modules/ol/View.js';
 
 const map = new Map({
     target: 'map',
