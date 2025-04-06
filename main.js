7c833ea60834f63998c6d5ef8cc743f832fbe59e
Paul Wittle <paul_wittle@hotmail.com>
2025-04-06 21:09:49 +0100

    Switching to OL

---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import Map from 'node_modules/ol/Map.js';
+import OSM from 'node_modules/ol/source/OSM.js';
+import TileLayer from 'node_modules/ol/layer/Tile.js';
+import View from 'node_modules/ol/View.js';
+
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
+    ],
+    view: new View({
+        center: [50.595111, -2.477780],
+        zoom: 15,
+    }),
+});
