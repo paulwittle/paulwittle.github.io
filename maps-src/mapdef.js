b46e6d02554e02c8c98ebe1bb23492e7bf41dd83
Paul Wittle <paul_wittle@hotmail.com>
2025-04-07 16:16:39 +0100

    Change style

--- maps-src/mapdef.js
+++ maps-src/mapdef.js
@@ -6,26 +6,43 @@ import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import Point from 'ol/geom/Point.js';
+import Icon from 'ol/style/Icon.js';
 import Stroke from 'ol/style/Stroke.js';
 import Style from 'ol/style/Style.js';
 
-const styles = {
-    'LineString': new Style({
-        stroke: new Stroke({
-            color: 'green',
-            width: 1,
-        }),
-    }),
-    'MultiLineString': new Style({
-        stroke: new Stroke({
-            color: 'green',
-            width: 1,
-        }),
-    })
-}
 
 const styleFunction = function (feature) {
-    return styles[feature.getGeometry().getType()];
+    const geometry = feature.getGeometry();
+    const styles = [
+        // linestring
+        new Style({
+            stroke: new Stroke({
+                color: '#a40d05',
+                width: 2,
+            }),
+        }),
+    ];
+
+    geometry.forEachSegment(function (start, end) {
+        const dx = end[0] - start[0];
+        const dy = end[1] - start[1];
+        const rotation = Math.atan2(dy, dx);
+        // arrows
+        styles.push(
+            new Style({
+                geometry: new Point(end),
+                image: new Icon({
+                    src: 'data/arrow.png',
+                    anchor: [0.75, 0.5],
+                    rotateWithView: true,
+                    rotation: -rotation,
+                }),
+            }),
+        );
+    });
+
+    return styles;
 };
 
 const fetchJSON = function(url) {
@@ -56,6 +73,6 @@ const map = new Map({
     ],
     view: new View({
         center: [-275816.0, 6549995.2],
-        zoom: 16,
+        zoom: 15,
     }),
 });
